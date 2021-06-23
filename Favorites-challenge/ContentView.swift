49e382c1b59a7894abc59043dc3c72e245966cb0
César González <cesar.gonzalez@ciberexperis.es>
2021-06-23 12:32:55 +0200

    Initial Commit

---
+++ Favorites-challenge/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Favorites-challenge
+//
+//  Created by César González Palomino on 23/6/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Favorites-challenge/Favorites_challengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Favorites_challengeApp.swift
+//  Favorites-challenge
+//
+//  Created by César González Palomino on 23/6/21.
+//
+
+import SwiftUI
+
+@main
+struct Favorites_challengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
